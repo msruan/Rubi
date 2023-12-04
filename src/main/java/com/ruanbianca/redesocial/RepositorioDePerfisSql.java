a6bbc621b7b859f180577c01a8ec3513e7c8a89c
RavCarvalho <ravennacarvalho098@gmail.com>
2023-12-04 15:47:26 -0300

    RPerfisSQL talvez concluído

--- src/main/java/com/ruanbianca/redesocial/RepositorioDePerfisSql.java
+++ src/main/java/com/ruanbianca/redesocial/RepositorioDePerfisSql.java
@@ -11,7 +11,7 @@ import java.util.Optional;
 import java.util.UUID;
 
 public class RepositorioDePerfisSql implements IRepositorioDePerfis {
-    
+
     private Connection conexao;
 
     public RepositorioDePerfisSql() {
@@ -22,124 +22,139 @@ public class RepositorioDePerfisSql implements IRepositorioDePerfis {
             String usuario = "root";
             String senha = "123456";
             conexao = DriverManager.getConnection(url, usuario, senha);
-        } catch (ClassNotFoundException | SQLException e){
+        } catch (ClassNotFoundException | SQLException e) {
             e.printStackTrace();
         }
     }
 
-    public void incluir(Perfil perfil) throws NullObjectAsArgumentException, UserAlreadyExistsException{
-        
+    public Optional<Perfil> consultarPerfil(UUID id, String username, String email) {
+
+        try {
+            ResultSet resultado = selectFromTabela("Perfil");
+
+            while (resultado.next()) {
+
+                if ((Optional.ofNullable(id).isPresent() &&
+                        id.equals(UUID.fromString(resultado.getString("id")))) ||
+                        (Optional.ofNullable(username).isPresent() &&
+                                username.equals(resultado.getString("username")))
+                        ||
+                        (Optional.ofNullable(email).isPresent() &&
+                                email.equals(resultado.getString("email")))) {
+                    Perfil perfil = new Perfil(UUID.fromString(resultado.getString("id")),
+                            resultado.getString("username"),
+                            resultado.getString("nome"),
+                            resultado.getString("email"),
+                            resultado.getString("biografia"));
+                    return Optional.ofNullable(perfil);
+                }
+            }
+            return Optional.empty();
+
+        } catch (SQLException e) {
+
+            System.err.println(
+                    "SQL não está funcionando no momento, por favor tente novamente com outro tipo de persistência...");
+            e.printStackTrace();
+            System.err.flush();
+            System.exit(1);
+            return Optional.empty();
+        }
+    }
+
+    public void incluir(Perfil perfil) throws NullObjectAsArgumentException, UserAlreadyExistsException {
+
         Optional.ofNullable(perfil).orElseThrow(NullObjectAsArgumentException::new);
 
-        if(usuarioJaExite(perfil.getId(), perfil.getUsername(),perfil.getEmail())){
+        if (usuarioJaExite(perfil.getId(), perfil.getUsername(), perfil.getEmail())) {
             throw new UserAlreadyExistsException();
         }
         try {
             String insert_sql = "INSERT INTO perfil(id, username, nome, email, biografia) VALUES (?,?,?,?,?)";
             PreparedStatement insert = conexao.prepareStatement(insert_sql);
-            insert.setString(1,perfil.getId().toString());
-            insert.setString(2,perfil.getUsername());
-            insert.setString(3,perfil.getNome());
-            insert.setString(4,perfil.getEmail());
-            insert.setString(5,perfil.getBiografia());
+            insert.setString(1, perfil.getId().toString());
+            insert.setString(2, perfil.getUsername());
+            insert.setString(3, perfil.getNome());
+            insert.setString(4, perfil.getEmail());
+            insert.setString(5, perfil.getBiografia());
             insert.executeUpdate();
-        } 
-        catch (SQLException e){
-            System.err.println("SQL não está funcionando no momento, por favor tente novamente com outro tipo de persistência..." + e.getMessage());
+        } catch (SQLException e) {
+            System.err.println(
+                    "SQL não está funcionando no momento, por favor tente novamente com outro tipo de persistência..."
+                            + e.getMessage());
             e.printStackTrace();
             System.err.flush();
             System.exit(1);
         }
     }
 
-
-    public Optional<Perfil> consultarPerfil(UUID id, String username, String email){
+    ResultSet selectFromTabela(String nomeTabela) throws SQLException {
 
         try {
-            ResultSet resultado = selectFromTabela("Perfil");
-
-            while(resultado.next()){
-               
-                if(
-                    ( Optional.ofNullable(id).isPresent() && 
-                        id.equals( UUID.fromString( resultado.getString("id") ) )) || 
-                    ( Optional.ofNullable(username).isPresent() && 
-                        username.equals(resultado.getString("username"))) ||
-                    ( Optional.ofNullable(email).isPresent() && 
-                        email.equals(resultado.getString("email")))
-                  )
-                {
-                        Perfil perfil = new Perfil(UUID.fromString(resultado.getString("id")), 
-                                    resultado.getString("username"),
-                                    resultado.getString("nome"), 
-                                    resultado.getString("email"), 
-                                    resultado.getString("biografia"));
-                        return Optional.ofNullable(perfil);
-                }             
-            }
-            return Optional.empty();
-            
-        }catch(SQLException e){
-
-            System.err.println("SQL não está funcionando no momento, por favor tente novamente com outro tipo de persistência...");
-            e.printStackTrace();
-            System.err.flush();
-            System.exit(1);
-            return Optional.empty();
-        }
-    }
-
-    
-    ResultSet selectFromTabela(String nomeTabela) throws SQLException{
-        
-        try{
-            ResultSet resultado = null; 
+            ResultSet resultado = null;
             Statement statement = conexao.createStatement();
 
             String querySelect = String.format("SELECT * FROM %s", nomeTabela);
             resultado = statement.executeQuery(querySelect);
             return resultado;
-            
-        }catch(SQLException e){
+
+        } catch (SQLException e) {
             throw e;
         }
     }
 
-    
-    public void removerPerfil(String username){
+    public void removerPerfil(String username) {
         try {
             String delete_sql = "DELETE FROM Perfil WHERE username = ?";
             PreparedStatement delete = conexao.prepareStatement(delete_sql);
-            delete.setString(1,username);
+            delete.setString(1, username);
             int afetado = delete.executeUpdate();
-            if(afetado == 0){
+            if (afetado == 0) {
                 throw new UserNotFoundException();
             }
-        } catch (SQLException e){
-            System.err.println("SQL não está funcionando no momento, por favor tente novamente com outro tipo de persistência..."+e.getMessage());
+        } catch (SQLException e) {
+            System.err.println(
+                    "SQL não está funcionando no momento, por favor tente novamente com outro tipo de persistência..."
+                            + e.getMessage());
             e.printStackTrace();
             System.err.flush();
             System.exit(1);
         }
     }
 
-
-    public boolean usuarioJaExite(UUID id, String username, String email){
+    public boolean usuarioJaExite(UUID id, String username, String email) {
 
         return consultarPerfil(id, username, email).isPresent();
     }
-    
 
-    public ArrayList<Perfil> getPerfis(){
-        return null;
-    }
+    public ArrayList<Perfil> getPerfis() {
 
+        try {
+            ResultSet resultado = selectFromTabela("Perfil");
 
-    public void resgatarPerfis(){
+            ArrayList<Perfil> perfis = new ArrayList<>();
+            Perfil perfil;
 
-    }
+            while (resultado.next()) {
 
-    public void salvarPerfis(){
+                perfil = new Perfil(UUID.fromString(
+                        resultado.getString("id")),
+                        resultado.getString("username"),
+                        resultado.getString("nome"),
+                        resultado.getString("email"),
+                        resultado.getString("biografia"));
+                perfis.add(perfil);
+            }
+            return perfis;
 
+        } catch (SQLException e) {
+
+            System.err.println(
+                    "SQL não está funcionando no momento, por favor tente novamente com outro tipo de persistência...");
+            e.printStackTrace();
+            System.err.flush();
+            System.exit(1);
+            return null;
+        }
     }
 }
